cd95162e484782b69a563e97ee238a5212414cf7
Morel K Data <129965534+Morelkdata@users.noreply.github.com>
2025-06-15 06:09:41 +0000

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,179 +1,258 @@
-!(function (e) {
-  var i,
-    s = e(window),
-    l = e("body"),
-    a = e("#wrapper"),
-    o = e("#header"),
-    n = e("#footer"),
-    t = e("#main"),
-    r = t.children("article");
-  breakpoints({
-    xlarge: ["1281px", "1680px"],
-    large: ["981px", "1280px"],
-    medium: ["737px", "980px"],
-    small: ["481px", "736px"],
-    xsmall: ["361px", "480px"],
-    xxsmall: [null, "360px"],
-  }),
-    s.on("load", function () {
-      window.setTimeout(function () {
-        l.removeClass("is-preload");
-      }, 100);
-    }),
-    "ie" == browser.name &&
-      s
-        .on("resize.flexbox-fix", function () {
-          clearTimeout(i),
-            (i = setTimeout(function () {
-              a.prop("scrollHeight") > s.height()
-                ? a.css("height", "auto")
-                : a.css("height", "100vh");
-            }, 250));
-        })
-        .triggerHandler("resize.flexbox-fix");
-  var c = o.children("nav"),
-    h = c.find("li");
-  h.length % 2 == 0 &&
-    (c.addClass("use-middle"), h.eq(h.length / 2).addClass("is-middle"));
-  var d = !1;
-  if (
-    ((t._show = function (e, i) {
-      var a = r.filter("#" + e);
-      if (0 != a.length) {
-        if (d || (void 0 !== i && !0 === i)) {
-          l.addClass("is-switching"),
-            l.addClass("is-article-visible"),
-            r.removeClass("active"),
-            o.hide(),
-            n.hide(),
-            t.show(),
-            a.show(),
-            a.addClass("active"),
-            (d = !1),
-            setTimeout(
-              function () {
-                l.removeClass("is-switching");
-              },
-              i ? 1e3 : 0
-            );
-          return;
-        }
-        if (((d = !0), l.hasClass("is-article-visible"))) {
-          var c = r.filter(".active");
-          c.removeClass("active"),
-            setTimeout(function () {
-              c.hide(),
-                a.show(),
-                setTimeout(function () {
-                  a.addClass("active"),
-                    s.scrollTop(0).triggerHandler("resize.flexbox-fix"),
-                    setTimeout(function () {
-                      d = !1;
-                    }, 325);
-                }, 25);
-            }, 325);
-        } else
-          l.addClass("is-article-visible"),
-            setTimeout(function () {
-              o.hide(),
-                n.hide(),
-                t.show(),
-                a.show(),
-                setTimeout(function () {
-                  a.addClass("active"),
-                    s.scrollTop(0).triggerHandler("resize.flexbox-fix"),
-                    setTimeout(function () {
-                      d = !1;
-                    }, 325);
-                }, 25);
-            }, 325);
-      }
-    }),
-    (t._hide = function (e) {
-      var i = r.filter(".active");
-      if (l.hasClass("is-article-visible")) {
-        if (
-          (void 0 !== e && !0 === e && history.pushState(null, null, "#"), d)
-        ) {
-          l.addClass("is-switching"),
-            i.removeClass("active"),
-            i.hide(),
-            t.hide(),
-            n.show(),
-            o.show(),
-            l.removeClass("is-article-visible"),
-            (d = !1),
-            l.removeClass("is-switching"),
-            s.scrollTop(0).triggerHandler("resize.flexbox-fix");
-          return;
-        }
-        (d = !0),
-          i.removeClass("active"),
-          setTimeout(function () {
-            i.hide(),
-              t.hide(),
-              n.show(),
-              o.show(),
-              setTimeout(function () {
-                l.removeClass("is-article-visible"),
-                  s.scrollTop(0).triggerHandler("resize.flexbox-fix"),
-                  setTimeout(function () {
-                    d = !1;
-                  }, 325);
-              }, 25);
-          }, 325);
-      }
-    }),
-    r.each(function () {
-      var i = e(this);
-      e('<div class="close">Close</div>')
-        .appendTo(i)
-        .on("click", function () {
-          location.hash = "";
-        }),
-        i.on("click", function (e) {
-          e.stopPropagation();
-        });
-    }),
-    l.on("click", function (e) {
-      l.hasClass("is-article-visible") && t._hide(!0);
-    }),
-    s.on("keyup", function (e) {
-      27 === e.keyCode && l.hasClass("is-article-visible") && t._hide(!0);
-    }),
-    s.on("hashchange", function (e) {
-      "" == location.hash || "#" == location.hash
-        ? (e.preventDefault(), e.stopPropagation(), t._hide())
-        : r.filter(location.hash).length > 0 &&
-          (e.preventDefault(),
-          e.stopPropagation(),
-          t._show(location.hash.substr(1)));
-    }),
-    "scrollRestoration" in history)
-  )
-    history.scrollRestoration = "manual";
-  else {
-    var f = 0,
-      v = 0,
-      u = e("html,body");
-    s.on("scroll", function () {
-      (f = v), (v = u.scrollTop());
-    }).on("hashchange", function () {
-      s.scrollTop(f);
-    });
-  }
-  t.hide(),
-    r.hide(),
-    "" != location.hash &&
-      "#" != location.hash &&
-      s.on("load", function () {
-        t._show(location.hash.substr(1), !0);
-      });
+/*
+	Massively by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper'),
+		$header = $('#header'),
+		$nav = $('#nav'),
+		$main = $('#main'),
+		$navPanelToggle, $navPanel, $navPanelInner;
+
+	// Breakpoints.
+		breakpoints({
+			default:   ['1681px',   null       ],
+			xlarge:    ['1281px',   '1680px'   ],
+			large:     ['981px',    '1280px'   ],
+			medium:    ['737px',    '980px'    ],
+			small:     ['481px',    '736px'    ],
+			xsmall:    ['361px',    '480px'    ],
+			xxsmall:   [null,       '360px'    ]
+		});
+
+	/**
+	 * Applies parallax scrolling to an element's background image.
+	 * @return {jQuery} jQuery object.
+	 */
+	$.fn._parallax = function(intensity) {
+
+		var	$window = $(window),
+			$this = $(this);
+
+		if (this.length == 0 || intensity === 0)
+			return $this;
+
+		if (this.length > 1) {
+
+			for (var i=0; i < this.length; i++)
+				$(this[i])._parallax(intensity);
+
+			return $this;
+
+		}
+
+		if (!intensity)
+			intensity = 0.25;
+
+		$this.each(function() {
+
+			var $t = $(this),
+				$bg = $('<div class="bg"></div>').appendTo($t),
+				on, off;
+
+			on = function() {
+
+				$bg
+					.removeClass('fixed')
+					.css('transform', 'matrix(1,0,0,1,0,0)');
+
+				$window
+					.on('scroll._parallax', function() {
+
+						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
+
+						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
+
+					});
+
+			};
+
+			off = function() {
+
+				$bg
+					.addClass('fixed')
+					.css('transform', 'none');
+
+				$window
+					.off('scroll._parallax');
+
+			};
+
+			// Disable parallax on ..
+				if (browser.name == 'ie'			// IE
+				||	browser.name == 'edge'			// Edge
+				||	window.devicePixelRatio > 1		// Retina/HiDPI (= poor performance)
+				||	browser.mobile)					// Mobile devices
+					off();
+
+			// Enable everywhere else.
+				else {
+
+					breakpoints.on('>large', on);
+					breakpoints.on('<=large', off);
+
+				}
+
+		});
+
+		$window
+			.off('load._parallax resize._parallax')
+			.on('load._parallax resize._parallax', function() {
+				$window.trigger('scroll');
+			});
+
+		return $(this);
+
+	};
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+	// Background.
+		$wrapper._parallax(0.925);
+
+	// Nav Panel.
+
+		// Toggle.
+			$navPanelToggle = $(
+				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
+			)
+				.appendTo($wrapper);
+
+			// Change toggle styling once we've scrolled past the header.
+				$header.scrollex({
+					bottom: '5vh',
+					enter: function() {
+						$navPanelToggle.removeClass('alt');
+					},
+					leave: function() {
+						$navPanelToggle.addClass('alt');
+					}
+				});
+
+		// Panel.
+			$navPanel = $(
+				'<div id="navPanel">' +
+					'<nav>' +
+					'</nav>' +
+					'<a href="#navPanel" class="close"></a>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right',
+					target: $body,
+					visibleClass: 'is-navPanel-visible'
+				});
+
+			// Get inner.
+				$navPanelInner = $navPanel.children('nav');
+
+			// Move nav content on breakpoint change.
+				var $navContent = $nav.children();
+
+				breakpoints.on('>medium', function() {
+
+					// NavPanel -> Nav.
+						$navContent.appendTo($nav);
+
+					// Flip icon classes.
+						$nav.find('.icons, .icon')
+							.removeClass('alt');
+
+				});
+
+				breakpoints.on('<=medium', function() {
+
+					// Nav -> NavPanel.
+						$navContent.appendTo($navPanelInner);
+
+					// Flip icon classes.
+						$navPanelInner.find('.icons, .icon')
+							.addClass('alt');
+
+				});
+
+			// Hack: Disable transitions on WP.
+				if (browser.os == 'wp'
+				&&	browser.osVersion < 10)
+					$navPanel
+						.css('transition', 'none');
+
+	// Intro.
+		var $intro = $('#intro');
+
+		if ($intro.length > 0) {
+
+			// Hack: Fix flex min-height on IE.
+				if (browser.name == 'ie') {
+					$window.on('resize.ie-intro-fix', function() {
+
+						var h = $intro.height();
+
+						if (h > $window.height())
+							$intro.css('height', 'auto');
+						else
+							$intro.css('height', h);
+
+					}).trigger('resize.ie-intro-fix');
+				}
+
+			// Hide intro on scroll (> small).
+				breakpoints.on('>small', function() {
+
+					$main.unscrollex();
+
+					$main.scrollex({
+						mode: 'bottom',
+						top: '25vh',
+						bottom: '-50vh',
+						enter: function() {
+							$intro.addClass('hidden');
+						},
+						leave: function() {
+							$intro.removeClass('hidden');
+						}
+					});
+
+				});
+
+			// Hide intro on scroll (<= small).
+				breakpoints.on('<=small', function() {
+
+					$main.unscrollex();
+
+					$main.scrollex({
+						mode: 'middle',
+						top: '15vh',
+						bottom: '-15vh',
+						enter: function() {
+							$intro.addClass('hidden');
+						},
+						leave: function() {
+							$intro.removeClass('hidden');
+						}
+					});
+
+			});
+
+		}
+
 })(jQuery);
-let name = document.getElementById("name"),
-  email = document.getElementById("email"),
-  message = document.getElementById("message"),
-  reset_click = document.getElementById("reset_click");
-reset_click.addEventListener("click", () => {
-  (name.value = ""), (email.value = ""), (message.value = "");
-});
